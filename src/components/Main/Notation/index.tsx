5c4dbebb0ba6367c3283c97e803a41041359f3f0
VietAnh <anhnguyenviet11299@gmail.com>
2023-04-10 21:24:41 +0700

    clean notation

--- src/components/Main/Notation/index.tsx
+++ src/components/Main/Notation/index.tsx
@@ -2,6 +2,8 @@ import { Box } from "@chakra-ui/react";
 import { CSSProperties, useMemo } from "react";
 import { useChess } from "../../../contexts/ChessContext";
 import { BLACK_ROWS, COLUMNS, WHITE_ROWS } from "../../../utils";
+import css from "./notation.module.css";
+import cn from "classnames";
 
 type NotationProps = {
   r: number;
@@ -22,14 +24,8 @@ export default function Notation({ r, c, isBlack }: NotationProps) {
     [orientation, c]
   );
 
-  const baseStyle = useMemo(
+  const colorStyle = useMemo(
     (): CSSProperties => ({
-      position: "absolute",
-      userSelect: "none",
-      padding: "0px 2px",
-      fontWeight: 600,
-      fontSize: 576 / 35,
-      zIndex: 2,
       color: isBlack
         ? squareStyle["default.light"]
         : squareStyle["default.dark"],
@@ -37,22 +33,28 @@ export default function Notation({ r, c, isBlack }: NotationProps) {
     [squareStyle]
   );
 
-  const alphaStyle = { alignSelf: "flex-end", right: 0 };
-
-  const numbericStyle = { alignSelf: "flex-start" };
-
-  if (r === 7 && c === 0)
-    return (
-      <>
-        <Box style={{ ...baseStyle, ...numbericStyle }}>{row}</Box>
-        <Box style={{ ...baseStyle, ...alphaStyle }}>{col}</Box>
-      </>
-    );
-
-  if (c === 0)
-    return <Box style={{ ...baseStyle, ...numbericStyle }}>{row}</Box>;
-
-  if (r === 7) return <Box style={{ ...baseStyle, ...alphaStyle }}>{col}</Box>;
-
-  return null;
+  return (
+    <>
+      {r === 7 && c === 0 && (
+        <>
+          <Box style={colorStyle} className={cn(css.notation, css.number)}>
+            {row}
+          </Box>
+          <Box style={colorStyle} className={cn(css.notation, css.alpha)}>
+            {col}
+          </Box>
+        </>
+      )}
+      {c === 0 && r !== 7 && (
+        <Box style={colorStyle} className={cn(css.notation, css.number)}>
+          {row}
+        </Box>
+      )}
+      {r === 7 && c !== 0 && (
+        <Box style={colorStyle} className={cn(css.notation, css.alpha)}>
+          {col}
+        </Box>
+      )}
+    </>
+  );
 }
