6e9152db4dd860f8a5f049647c9db4e975e999bd
VietAnh <anhnguyenviet11299@gmail.com>
2023-06-18 22:26:49 +0700

    update

--- src/components/Sidebar/AI/Moves/index.tsx
+++ src/components/Sidebar/AI/Moves/index.tsx
@@ -3,7 +3,7 @@ import MoveList from "../../Common/Moves/MoveList";
 
 export default function MoveContainer() {
   return (
-    <Flex flex="4" h="full" direction="column" overflow="hidden">
+    <Flex flex="4 1 0" minH="0" direction="column" overflow="hidden">
       <Box overflow="auto" overflowY="scroll">
         <MoveList />
       </Box>
