ae42c015a0c5ebab834430566b6ca4453c4ab209
VietAnh <anhnguyenviet11299@gmail.com>
2023-06-18 10:27:10 +0700

    update

--- src/components/Sidebar/Multiplayer/Play/PlayMoves.tsx
+++ src/components/Sidebar/Multiplayer/Play/PlayMoves.tsx
@@ -4,7 +4,7 @@ import MoveList from "../../Common/Moves/MoveList";
 export default function MoveContainer() {
   return (
     <Flex flex="1 1 0" direction="column" minH="0">
-      <Box overflow="auto">
+      <Box overflow="auto" overflowY="scroll">
         <MoveList />
       </Box>
     </Flex>
